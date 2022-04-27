278ed3140d114c8ec72c0ad51a4c60d82ec4f66e
Marc Gavanier <marc.gavanier@gmail.com>
2022-04-27 16:19:55 +0200

    fix s3 acl require dedicated resource

--- s3.tf
+++ s3.tf
@@ -1,6 +1,10 @@
 resource "aws_s3_bucket" "client_driver" {
   bucket        = "${local.product_information.context.project}-${local.product_information.context.service}"
-  acl           = "private"
   force_destroy = true
   tags          = local.tags
+}
+
+resource "aws_s3_bucket_acl" "example_bucket_acl" {
+  bucket = aws_s3_bucket.client_driver.id
+  acl    = "private"
 }
