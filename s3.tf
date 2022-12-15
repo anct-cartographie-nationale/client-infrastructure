d9e7ebbf3b7534ce4a77a876bc6273b86e7a360a
Marc Gavanier <marc.gavanier@gmail.com>
2022-12-15 11:03:26 +0100

    feat: add public access policy for s3 website

--- s3.tf
+++ s3.tf
@@ -9,7 +9,7 @@ resource "aws_s3_bucket_acl" "bucket_acl" {
   acl    = "private"
 }
 
-resource "aws_s3_bucket_website_configuration" "example" {
+resource "aws_s3_bucket_website_configuration" "client_website" {
   count  = var.enable_bucket_website ? 1 : 0
   bucket = aws_s3_bucket.client.bucket
 
@@ -19,5 +19,26 @@ resource "aws_s3_bucket_website_configuration" "example" {
 
   error_document {
     key = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "allow_client_website_access_from_internet" {
+  count  = var.enable_bucket_website ? 1 : 0
+  bucket = aws_s3_bucket.client.id
+  policy = data.aws_iam_policy_document.allow_client_website_access_from_internet.json
+}
+
+data "aws_iam_policy_document" "allow_client_website_access_from_internet" {
+  statement {
+    sid    = "PublicReadGetObject"
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.client.arn}/*",
+    ]
   }
 }
