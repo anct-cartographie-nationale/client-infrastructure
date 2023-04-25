0088582122e0cef5b4697fc7cc64db56e025850c
Marc Gavanier <marc.gavanier@gmail.com>
2023-04-25 18:41:06 +0200

    fix: add client website bucket ownership and acl

--- s3.tf
+++ s3.tf
@@ -22,7 +22,15 @@ resource "aws_s3_bucket_website_configuration" "client_website" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "example" {
+resource "aws_s3_bucket_ownership_controls" "client_website_bucket_ownership" {
+  count  = var.enable_bucket_website ? 1 : 0
+  bucket = aws_s3_bucket.client.bucket
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "client_website_bucket_public_access" {
   count  = var.enable_bucket_website ? 1 : 0
   bucket = aws_s3_bucket.client.id
 
@@ -30,6 +38,17 @@ resource "aws_s3_bucket_public_access_block" "example" {
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "client_website_bucket_acl" {
+  count  = var.enable_bucket_website ? 1 : 0
+  depends_on = [
+    aws_s3_bucket_ownership_controls.client_website_bucket_ownership,
+    aws_s3_bucket_public_access_block.client_website_bucket_public_access,
+  ]
+
+  bucket = aws_s3_bucket.client.bucket
+  acl    = "public-read"
 }
 
 resource "aws_s3_bucket_policy" "allow_client_website_access_from_internet" {
